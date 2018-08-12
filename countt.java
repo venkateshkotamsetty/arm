b8a6ddc2917c2fa02a9a10cfc91907c130289fbb
venkateshkotamsetty <41735214+venkateshkotamsetty@users.noreply.github.com>
2018-08-12 19:56:38 +0530

    Create countt.java

---
+++ countt.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num,count=0;
+		Scanner s = new Scanner(System.in);
+		num = s.nextInt();
+		while(num!=0)
+		{
+			num=num/10;
+			++count;
+		}
+	
+		System.out.println(count);
+	}
+}
